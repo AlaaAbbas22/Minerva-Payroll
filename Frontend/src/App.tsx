f3e1088c3556079d34126ca7f543230ba4de8c74
AlaaAbbas22 <alaa@uni.minerva.edu>
2024-04-14 11:31:09 +0200

    edit api link

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -25,7 +25,8 @@ import ManagerProfile from "./components/ManagerProfile";
 
 
 //const baseURL = "http://127.0.0.1:8000"//
-const baseURL ="https://minerva-payroll.onrender.com"
+//const baseURL ="https://minerva-payroll.onrender.com"
+const baseURL = "https://minerva-payroll-sggn.onrender.com"
 
 
 async function handleLogOut() {
